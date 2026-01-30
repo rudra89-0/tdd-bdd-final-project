9d1a1b6a3ab25812ff39a54fe8ff8395bb3f49ac
Brendan Rodrigues <rodriguesbrendan644@gmail.com>
2026-01-30 10:21:10 -0500

    Fix duplicate list_products route

--- service/routes.py
+++ service/routes.py
@@ -171,24 +171,6 @@ def update_products(product_id):
 ######################################################################
 # LIST PRODUCTS
 ######################################################################
-@app.route("/products", methods=["GET"])
-def list_products():
-    """Returns a list of Products"""
-    app.logger.info("Request to list Products...")
-
-    products = []
-    name = request.args.get("name")
-
-    if name:
-        app.logger.info("Find by name: %s", name)
-        products = Product.find_by_name(name)
-    else:
-        app.logger.info("Find all")
-        products = Product.all()
-
-    results = [product.serialize() for product in products]
-    app.logger.info("[%s] Products returned", len(results))
-    return results, status.HTTP_200_OK
 
 ######################################################################
 # LIST PRODUCTS
